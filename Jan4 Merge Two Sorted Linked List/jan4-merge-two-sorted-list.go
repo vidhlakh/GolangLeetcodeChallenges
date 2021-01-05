cac6c41f4f2c70b536b5f2b4823133a230f919a7
vidhlakh <vidhlakh@gmail.com>
2021-01-04 21:03:02 -0500

    Jan 4 Merge sorted linked list

---
+++ Jan4 Merge Two Sorted Linked List/jan4-merge-two-sorted-list.go
@@ -0,0 +1,61 @@
+package main
+
+// ListNode Singly linkedlist
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	// var result *ListNode
+	// for {
+	//     if l1 != nil && l2 != nil{
+
+	//         if l1.Val <= l2.Val{
+	//             result.Next = l1
+	//             l1 = l1.Next
+	//         }else{
+	//             result.Next = l2
+	//             l2 = l2.Next
+	//         }
+	//     }else{
+	//         break
+	//     }
+	//     result= result.Next
+	// }
+	//     if l1 != nil {
+	// 	result.Next = l1
+	//     }
+	//     if l2 != nil {
+	//        result.Next = l2
+	//     }
+
+	// return result
+	var a *ListNode
+	pos := &ListNode{Val: -1, Next: nil}
+	a = pos
+	for {
+		if l1 != nil && l2 != nil {
+
+			if l1.Val <= l2.Val {
+				pos.Next = l1
+				l1 = l1.Next
+			} else {
+				pos.Next = l2
+				l2 = l2.Next
+			}
+			pos = pos.Next
+		} else {
+			break
+		}
+
+	}
+	if l1 != nil {
+		pos.Next = l1
+	}
+	if l2 != nil {
+		pos.Next = l2
+	}
+
+	return a.Next
+}
